fa7e1fd0ef049ebad31e42675fa87fda2d567698
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-10-11 14:28:26 -0500

    use official Oracle Java base

--- jetty.sh
+++ jetty.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-JETTY_HOME=/usr/share/jetty
-LOGDIR=/var/log/jetty
+JETTY_HOME=/usr/share/jetty8
+LOGDIR=/var/log/jetty8
 
 /usr/bin/java -Djetty.home=$JETTY_HOME -Djetty.base=$JETTY_HOME -Djetty.logs=$LOGDIR -jar $JETTY_HOME/start.jar
